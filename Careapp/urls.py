edfe13f66bc541a0a18924f567bc46646595bc89
848bruno <ambalebruno848@gmail.com>
2025-11-21 16:58:27 +0300

    added extra pages

--- Careapp/urls.py
+++ Careapp/urls.py
@@ -8,6 +8,22 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path('starter/', views.starter, name='starter-page'),
+    path('blog/', views.blog, name='blog'),
+    path('event/', views.event, name='event'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('careers/', views.careers, name='careers'),
+    path('contact/', views.contact, name='contact'),
+    path('appointment/', views.appointment, name='appointment'),
+    path('billing/', views.billing, name='billing'),
+    path('departments/', views.departments, name='departments'),
+    path('doctors/', views.doctors, name='doctors'),
+    path('faq/', views.faq, name='faq'),
+    path('healthTips/', views.healthTips, name='healthTips'),
+    path('insuranceInfo/', views.insuranceInfo, name='insuranceInfo'),
+    path('medicalRecords/', views.medicalRecords, name='medicalRecords'),
+    path('patientForms/', views.patientForms, name='patientForms'),
+
 
    
 ]
--- Careapp/views.py
+++ Careapp/views.py
@@ -6,3 +6,51 @@ def index(request):
 
 def starter(request):
     return render(request, 'starter-page.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def event(request):
+    return render(request, 'event.html')
+
+def about(request): 
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def careers(request):
+    return render(request, 'careers.html')
+
+def contact(request):
+    return render(request, 'contact.html')  
+
+def appointment(request):
+    return render(request, 'appointment.html')
+
+def billing(request):
+    return render(request, 'billing.html')
+
+def departments(request):
+    return render(request, 'departments.html')
+
+def doctors(request):
+    return render(request, 'doctors.html')
+
+def faq(request):
+    return render(request, 'faq.html')
+
+def healthTips(request):
+    return render(request, 'healthTips.html')
+
+def insuranceInfo(request):
+    return render(request, 'insuranceInfo.html')
+
+def medicalRecords(request):
+    return render(request, 'medicalRecords.html')
+
+def patientForms(request):
+    return render(request, 'patientForms.html')
+
+
+
