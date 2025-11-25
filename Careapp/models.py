c7f3910b5fe1060ad7a6a52e61034d63f3acd266
848bruno <ambalebruno848@gmail.com>
2025-11-25 15:14:59 +0300

    updated models.py amd admin.py

--- Careapp/models.py
+++ Careapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_of_birth = models.DateField()
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    
+    
